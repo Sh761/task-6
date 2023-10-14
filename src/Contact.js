23f8446280b4bb5853e88a437dfc632114e2dbeb
Shreya <shreyabatra003@gmail.com>
2023-10-14 22:39:53 +0530

    first commit

---
+++ src/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Me</h1>
+      <p>This is the contact page of my portfolio website.</p>
+    </div>
+  );
+}
+
+export default Contact;
